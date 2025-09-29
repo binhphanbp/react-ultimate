0ea03dd569a110e2ef56745254ab47ae3b708113
binhphan <phanbinhfedev150504@gmail.com>
2025-09-29 08:04:42 +0700

    Arrow Function (#27)

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
+
+  // function myFunction() {
+  //   console.log('Run my Function');
+  // }
+  // myFunction();
+  const myArrowFunction = () => {
+    console.log('Run my Arrow Function');
+  };
+  myArrowFunction();
 
   return (
     <>
@@ -30,6 +39,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
