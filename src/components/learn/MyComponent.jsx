d160d3bbe8a427dad7ab8f5c6f1ea349134d4320
binhphan <phanbinhfedev150504@gmail.com>
2025-09-29 09:40:50 +0700

    JSX; Fragment; style with className

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,14 @@
+// JSX: 1 parent element
+// Fragment <></> tránh render HTML thừa thãi
+import './style.css';
+
 const MyComponent = () => {
-  return <div>BinhPhan's First Component</div>;
+  return (
+    <>
+      <div>BinhPhan's First Component</div>
+      <div className="child">Child</div>
+    </>
+  );
 };
 
 export default MyComponent;
