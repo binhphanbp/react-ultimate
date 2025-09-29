377f49e18796aaaeac3c5e4f7c7321d7465e89ff
binhphan <phanbinhfedev150504@gmail.com>
2025-09-29 11:38:29 +0700

    Use variables with JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,24 @@
 import './style.css';
 
 const MyComponent = () => {
+  // const binhPhan = 'Bình Phan'; // string
+  // const binhPhan = 21; // number
+  // const binhPhan = true; // boolean
+  // const binhPhan = undefined; // undefined
+  // const binhPhan = null; // null
+  // const binhPhan = [1, 2, 3];
+  const binhPhan = {
+    name: 'Bình Phan',
+    age: 21,
+  };
+
   return (
     <>
-      <div>BinhPhan's First Component</div>
-      <div className="child">Child</div>
+      <div>{JSON.stringify(binhPhan)} & First Component</div>
+      <div>{console.log('Bình Phan console.log')}</div>
+      <div className="child" style={{ borderRadius: '10px' }}>
+        Child
+      </div>
     </>
   );
 };
