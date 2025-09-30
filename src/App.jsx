50196092ad6b8a74e7251cd5f317619af03684b0
binhphan <phanbinhfedev150504@gmail.com>
2025-09-30 19:23:23 +0700

    Call parent's function from a child component (#35)

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,15 @@ const App = () => {
     country: 'Vietnam',
   };
 
+  const addNewTodo = (name) => {
+    alert(`Hello ${name}`);
+  };
+  // addNewTodo();
+
   return (
     <div className="todo-container">
       <p className="todo-title">Todo List</p>
-      <TodoNew />
+      <TodoNew addNewTodo={addNewTodo} />
       <TodoData name={binhPhan} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,4 +1,8 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+  console.log('>>> Check props:', props);
+  const { addNewTodo } = props;
+
+  // addNewTodo('Bình Phan');
   return (
     <div className="todo-new">
       <input type="text" />
