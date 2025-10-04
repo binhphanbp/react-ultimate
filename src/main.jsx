712fde402906118977d482cdfe5febc4f9cec598
binhphan <phanbinhfedev150504@gmail.com>
2025-10-04 22:47:07 +0700

    Project Structure (#46)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import ProductPage from './pages/products.jsx';
+import UsersPage from './pages/users.jsx';
+import './styles/global.css';
 
 const router = createBrowserRouter([
   {
@@ -10,15 +15,19 @@ const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <div>Login Page</div>,
+    element: <LoginPage />,
   },
   {
     path: '/register',
-    element: <div>Register Page</div>,
+    element: <RegisterPage />,
   },
   {
     path: '/products',
-    element: <div>Products Page</div>,
+    element: <ProductPage />,
+  },
+  {
+    path: '/users',
+    element: <UsersPage />,
   },
 ]);
 
