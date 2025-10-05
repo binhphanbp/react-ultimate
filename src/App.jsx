914c455def6684041694d10852d4883b57bf6afd
binhphan <phanbinhfedev150504@gmail.com>
2025-10-05 08:56:45 +0700

    Create Header & Footer (#47)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 const App = () => {
   const [todoList, setTodoList] = useState([
@@ -41,18 +43,22 @@ const App = () => {
   };
 
   return (
-    <div className="todo-container">
-      <p className="todo-title">Todo List</p>
-      <TodoNew addNewTodo={addNewTodo} />
+    <>
+      <Header />
+      <div className="todo-container">
+        <p className="todo-title">Todo List</p>
+        <TodoNew addNewTodo={addNewTodo} />
 
-      {todoList.length > 0 ? (
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-      ) : (
-        <div className="todo-image">
-          <img src={reactLogo} className="logo" />
-        </div>
-      )}
-    </div>
+        {todoList.length > 0 ? (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        ) : (
+          <div className="todo-image">
+            <img src={reactLogo} className="logo" />
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
   );
 };
 
