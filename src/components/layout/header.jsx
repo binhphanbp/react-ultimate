d4c17536c57de85e576ab7462c8c77c45c74d64c
binhphan <phanbinhfedev150504@gmail.com>
2025-10-13 07:57:23 +0700

    Update Header with Header Component from Ant Design (#76)

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,21 +1,40 @@
 import { Link, NavLink } from 'react-router-dom';
-import './header.css';
+import { Menu } from 'antd';
+import { BookOutlined, HomeOutlined, UserAddOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 const Header = () => {
+  const [current, setCurrent] = useState('');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  const items = [
+    {
+      label: <Link to={'/'}>Home</Link>,
+      key: 'home',
+      icon: <HomeOutlined />,
+    },
+    {
+      label: <Link to={'/users'}>Users</Link>,
+      key: 'users',
+      icon: <UserAddOutlined />,
+    },
+    {
+      label: <Link to={'/books'}>Books</Link>,
+      key: 'books',
+      icon: <BookOutlined />,
+    },
+  ];
+
   return (
-    <ul>
-      <li>
-        <NavLink className="active" to="/">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/users">Users</NavLink>
-      </li>
-      <li>
-        <NavLink to="/books">Books</NavLink>
-      </li>
-    </ul>
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
   );
 };
 
